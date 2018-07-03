18f190cdbf1b473fa1a3b9a2a7f7b110d48ad47b
Daniyar Salikhov <salihovdr@gmail.com>
2018-07-03 15:06:09 -0400

    complete queries.js drills

--- queries.js
+++ queries.js
@@ -70,10 +70,78 @@ process.stdout.write('\x1Bc');
 //   .where({address_zipcode: '11372', cuisine: 'Thai' })
 //   .then(res => console.log(res));
 
-knex('restaurants')
-  .select('id', 'name')
-  .where('cuisine', 'Italian')
-  .whereIn('address_zipcode', [10012, 10013, 10014])
-  .orderBy('name', 'asc')
-  .limit(5)
-  .then(res => console.log(res));
+// knex('restaurants')
+//   .select('id', 'name')
+//   .where('cuisine', 'Italian')
+//   .whereIn('address_zipcode', [10012, 10013, 10014])
+//   .orderBy('name', 'asc')
+//   .limit(5)
+//   .then(res => console.log(res));
+
+
+// knex('restaurants')
+//   .insert({name: 'Dummy Cafe',
+//     borough: 'Brooklyn',
+//     cuisine: 'coffee',
+//     address_building_number: '123',
+//     address_street: 'Atlantic Avenue',
+//     address_zipcode: '11231'
+//   })
+//   .then(res=>console.log(res));
+
+
+// knex('restaurants')
+//   .insert({name: 'Coffee Place',
+//     borough: 'Manhattan',
+//     cuisine: 'coffee',
+//     address_building_number: '234',
+//     address_street: 'Uzbekistan Avenue',
+//     address_zipcode: '11101'
+//   }, ['id', 'name'])
+//   .then(res=>console.log(res));
+
+// knex('restaurants')
+//   .insert([{ name: 'Pie Republic',
+//     borough: 'Brooklyn',
+//     cuisine: 'coffee',
+//     address_building_number: '555',
+//     address_street: 'Virginia Boulevard',
+//     address_zipcode: '11234'
+//   },
+//   {
+//     name: 'Yet Another Cafe',
+//     borough: 'Brooklyn',
+//     cuisine: 'coffee',
+//     address_building_number: '789',
+//     address_street: 'Arlington Boulevard',
+//     address_zipcode: '34765'
+//   },
+//   {
+//     name: 'Pancake Heaven',
+//     borough: 'Manhattan',
+//     cuisine: 'coffee',
+//     address_building_number: '435',
+//     address_street: 'Washington Street',
+//     address_zipcode: '34345'
+//   }], ['id', 'name'])
+//   .then(res => console.log(res));
+
+// knex('restaurants')
+//   .where('nyc_restaurant_id', '30191841')
+//   .update('name', 'DJ Reynolds Pub and Restaurant', ['id', 'name'])
+//   .then(res=> console.log(res));
+
+// knex('grades')
+//   .where('id', '10')
+//   .del()
+//   .then(res=>console.log(res));
+
+// knex('restaurants')
+//   .where('id', '22')
+//   .del()
+//   .then(res=>console.log(res));
+
+// knex('grades')
+//   .select()
+//   .where('id', '10')
+//   .then(res => console.log(res));

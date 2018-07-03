d80d659be92573e27b6c9502372633cce7c143ea
Daniyar Salikhov <salihovdr@gmail.com>
2018-07-03 11:49:57 -0400

    initial commit

--- queries.js
+++ queries.js
@@ -7,9 +7,35 @@ const knex = require('knex')(DATABASE);
 process.stdout.write('\x1Bc');
 
 // Sample select 
-knex
-  .select()
-  .from('restaurants')
-  .limit(2)
-  .debug(true)
-  .then(results => console.log(JSON.stringify(results, null, 2)));
+// knex('restaurants')
+//   .select('id', 'name', 'borough', 'cuisine')
+//   .where('borough', 'Manhattan')
+//   .limit(5)
+//   .debug(false)
+//   .then(results => console.log(results));
+
+// .then(results => console.log(JSON.stringify(results, null, 2)));
+
+// knex
+//   .select()
+//   .table('restaurants')
+//   .limit(2)
+//   .debug(false)
+//   .then(results => console.log(results));
+
+// knex
+//   .select('name', 'cuisine', 'address_zipcode')
+//   .from('restaurants')
+//   .limit(10)
+//   .then(res => console.log(res));
+
+// knex
+//   .column('name', 'cuisine', 'address_zipcode')
+//   .from('restaurants')
+//   .limit(5)
+//   .then(res => console.log(JSON.stringify(res, null, 2)));
+
+// knex('restaurants')
+//   .where({cuisine: 'Hamburgers'})
+//   .select('id')
+//   .then(res => console.log(res));
